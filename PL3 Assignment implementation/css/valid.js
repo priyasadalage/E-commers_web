0ec9ff9caa4b0e0d4559f4e9533967324f45a4d1
priyasadalage <110296521+priyasadalage@users.noreply.github.com>
2022-09-19 21:46:38 +0530

    Add files via upload

---
+++ PL3 Assignment implementation/css/valid.js
@@ -0,0 +1,25 @@
+function myfunction(){
+    var x =document.getElementById("pass");
+
+    if(x.type === "password"){
+        x.type = "text";
+    }
+    else{
+        x.type = "password";
+    }
+}
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.value.length >= 8){
+        alert("Login Succesfull");
+        window.location.replace("welcome.html");
+        return false;
+    }
+    else{
+        alert("Login Failed Password Must Be Greater than 8 Digits");
+    }
+}
+
